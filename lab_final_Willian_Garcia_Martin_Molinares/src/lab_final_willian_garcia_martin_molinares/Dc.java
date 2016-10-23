8a000429e03dbe24d4e1cfca96b34627109ae0ca
Willian Garcia <wgarcia1309@hotmail.com>
2016-10-23 01:31:34 -0500

    Cifrado
    Hacer operacion inversa, clave mayuscula nos envia error,verficicar
    funcionamiento con varias palabras, cifrar a binario,Expresiones
    Regulares,crear nuestro cifrado

--- lab_final_Willian_Garcia_Martin_Molinares/src/lab_final_willian_garcia_martin_molinares/Dc.java
+++ lab_final_Willian_Garcia_Martin_Molinares/src/lab_final_willian_garcia_martin_molinares/Dc.java
@@ -1,4 +1,5 @@
 package lab_final_willian_garcia_martin_molinares;
+
 import java.util.StringTokenizer;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -9,7 +10,7 @@ import javax.swing.JPanel;
 import org.jvnet.substance.SubstanceLookAndFeel;
 
 public class Dc extends javax.swing.JFrame {
-
+    
     public Dc() {
         initComponents();
         JFrame.setDefaultLookAndFeelDecorated(true);
@@ -19,13 +20,12 @@ public class Dc extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setResizable(false);
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
-        jDesktopPane1 = new javax.swing.JDesktopPane();
         jPanel1 = new javax.swing.JPanel();
         txt1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
@@ -33,10 +33,9 @@ public class Dc extends javax.swing.JFrame {
         Bie = new javax.swing.JRadioButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jDesktopPane1.setOpaque(false);
 
         jPanel1.setOpaque(false);
 
@@ -69,6 +68,13 @@ public class Dc extends javax.swing.JFrame {
             }
         });
 
+        jButton4.setText("Encriptar");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -85,8 +91,10 @@ public class Dc extends javax.swing.JFrame {
                     .addComponent(txt1, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(83, 83, 83)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -105,39 +113,26 @@ public class Dc extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-
-        jDesktopPane1.setLayer(jPanel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
-
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(11, Short.MAX_VALUE))
-        );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, 81, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         pack();
@@ -147,35 +142,36 @@ public class Dc extends javax.swing.JFrame {
     public static String[] II = {"hello", "phone", "red", "shoe", "sky", "world", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
     public static String[] EI = {"hola", "telefono", "rojo", "zapato", "cielo", "mundo", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
     public static int act = 6;
+    public static char[] letras = new char[27];
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        if(!(Bie.isSelected())&& !(Bei.isSelected())){
+        if (!(Bie.isSelected()) && !(Bei.isSelected())) {
             JOptionPane.showMessageDialog(null, "Error Seleccione una opcion");
-        }else{
-        String po = (txt1.getText()).toLowerCase();
-        po = po.replaceAll(" ", "");
-        StringTokenizer st = new StringTokenizer(po, ",");
-        String tr = "";
-        while (st.hasMoreTokens() && !(po.equals(""))) {
-            String p = st.nextToken();
-            if (!(p.equals(""))) {
-                if (Bie.isSelected() == true) {
-                    tr = tr + traduce_to_Ingles(p) + ",";
-                } else if (Bei.isSelected() == true) {
-                    tr = tr + traduce_to_Español(p) + ",";
+        } else {
+            String po = (txt1.getText()).toLowerCase();
+            po = po.replaceAll(" ", "");
+            StringTokenizer st = new StringTokenizer(po, ",");
+            String tr = "";
+            while (st.hasMoreTokens() && !(po.equals(""))) {
+                String p = st.nextToken();
+                if (!(p.equals(""))) {
+                    if (Bie.isSelected() == true) {
+                        tr = tr + traduce_to_Ingles(p) + ",";
+                    } else if (Bei.isSelected() == true) {
+                        tr = tr + traduce_to_Español(p) + ",";
+                    }
                 }
             }
-        }
-        po = (txt1.getText()).toLowerCase();
-        po = po.replaceAll(" ", "");
-        StringTokenizer vr = new StringTokenizer(po, ",");
-        if (po.equals("")) {
-            JOptionPane.showMessageDialog(null, "No ha ingresado nada");
-        } else if (vr.countTokens() > 4) {
-            JOptionPane.showMessageDialog(null, "Excedio el numero de palabras");
-        } else if (vr.countTokens() <= 4 && !(po.equals(""))) {
-            JOptionPane.showMessageDialog(null, tr.substring(0, tr.length() - 1));
-        }
+            po = (txt1.getText()).toLowerCase();
+            po = po.replaceAll(" ", "");
+            StringTokenizer vr = new StringTokenizer(po, ",");
+            if (po.equals("")) {
+                JOptionPane.showMessageDialog(null, "No ha ingresado nada");
+            } else if (vr.countTokens() > 4) {
+                JOptionPane.showMessageDialog(null, "Excedio el numero de palabras");
+            } else if (vr.countTokens() <= 4 && !(po.equals(""))) {
+                JOptionPane.showMessageDialog(null, tr.substring(0, tr.length() - 1));
+            }
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 //añadir
@@ -198,6 +194,49 @@ public class Dc extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, "El diccionario esta vacio");
         }
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        if (!(Bie.isSelected()) && !(Bei.isSelected())) {
+            JOptionPane.showMessageDialog(null, "Error seleccione una opcion");
+        } else {
+            String po = (txt1.getText()).toLowerCase();
+            po = po.replaceAll(" ", "");
+            StringTokenizer st = new StringTokenizer(po, ",");
+            String tr = "";
+            while (st.hasMoreTokens() && !(po.equals(""))) {
+                String p = st.nextToken();
+                if (!(p.equals(""))) {
+                    if (Bie.isSelected() == true) {
+                        if (traduce_to_Ingles(p).equals("No tenemos la traduccion")) {
+                            JOptionPane.showMessageDialog(null, "No es pobile encriptar");
+                            tr = "";
+                            break;
+                        }
+                        tr = tr + traduce_to_Ingles(p) + ",";
+                    } else if (Bei.isSelected() == true) {
+                        if (traduce_to_Español(p).equals("No tenemos la traduccion")) {
+                            JOptionPane.showMessageDialog(null, "No es pobile encriptar");
+                            tr = "";
+                            break;
+                        }
+                        tr = tr + traduce_to_Español(p) + ",";
+                    }
+                }
+            }
+            po = (txt1.getText()).toLowerCase();
+            po = po.replaceAll(" ", "");
+            StringTokenizer vr = new StringTokenizer(po, ",");
+            if (po.equals("")) {
+                JOptionPane.showMessageDialog(null, "No ha ingresado nada");
+            } else if (vr.countTokens() > 4) {
+                JOptionPane.showMessageDialog(null, "Excedio el numero de palabras");
+            } else if (vr.countTokens() <= 4 && !(po.equals(""))) {
+                if (!tr.equals("")) {
+                    Encriptar(tr.substring(0, tr.length() - 1));
+                }
+            }
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
     public static void deleteEE(String p) {
         for (int i = buscar(p, EE); i < EE.length - 1; i++) {
             EE[i] = EE[i + 1];
@@ -207,8 +246,71 @@ public class Dc extends javax.swing.JFrame {
             EI[i] = EI[i + 1];
             II[i] = II[i + 1];
         }
-    }    
-     public static void deleteII(String p) {
+    }
+    
+    public static void Encriptar(String s) {
+        int a = (int) 'a';
+        for (int i = 0; i < letras.length; i++) {
+            if (i < 14) {
+                letras[i] = (char) (a + i);
+            } else if (i == 14) {
+                letras[i] = 'ñ';
+            } else {
+                letras[i] = (char) (a - 1 + i);
+            }
+        }
+        String clave = "";
+        do {
+            clave = JOptionPane.showInputDialog(null, "Digite la clave");
+        } while (clave == null || clave.equals(""));
+        if (clave.length() < s.length()) {
+            clave = igclave(s, clave);
+        }
+        s = s.replaceAll(" ", "");
+        char f1[], cl[];
+        f1 = s.toCharArray();
+        cl = clave.toCharArray();       
+        for (int i = 0; i < s.length(); i++) {
+            if (f1[i] == ',') {
+                f1[i] = cl[i];
+            } else {
+                int n = (posletra(f1[i]) + posletra(cl[i])) % 27;
+                f1[i] = letras[n];
+            }
+        }
+        String lol = "";
+        for (int i = 0; i < s.length(); i++) {
+            lol = lol + f1[i];
+        }
+        JOptionPane.showMessageDialog(null, lol);
+    }
+
+    public static String igclave(String w, String c) {
+            int cont = 0;
+            int p = c.length();
+            for (int i = p; i < w.length(); i++) {
+                if (cont == p) {
+                    cont = 0;
+                }
+                if (!w.substring(i,i+1).equals(',')) {
+                    c = c + "" + c.substring(cont, cont + 1);
+                    cont++;
+                } else {
+                    c = c + " " + c.substring(cont, cont + 1);
+                }
+            }
+        return c;
+    }
+
+    public static int posletra(char l) {
+        int i = 0;
+        while (i < 27 && letras[i] != l) {
+            i++;
+        }
+        return i;
+    }
+    
+    public static void deleteII(String p) {
         for (int i = buscar(p, IE); i < IE.length - 1; i++) {
             EE[i] = EE[i + 1];
             IE[i] = IE[i + 1];
@@ -217,8 +319,8 @@ public class Dc extends javax.swing.JFrame {
             EI[i] = EI[i + 1];
             II[i] = II[i + 1];
         }
-    }    
-
+    }
+    
     public static void añadirII(String p, String p1) {
         int x = posicion(p1, II);
         for (int i = EI.length - 1; i > x; i--) {
@@ -228,7 +330,7 @@ public class Dc extends javax.swing.JFrame {
         EI[x] = p;
         II[x] = p1;
     }
-
+    
     public static void añadirEE(String p, String p1) {
         int x = posicion(p, EE);
         for (int i = EE.length - 1; i > x; i--) {
@@ -238,7 +340,7 @@ public class Dc extends javax.swing.JFrame {
         EE[x] = p;
         IE[x] = p1;
     }
-
+    
     public static int posicion(String p, String a[]) {
         int x = 0;
         while (x < a.length) {
@@ -249,7 +351,7 @@ public class Dc extends javax.swing.JFrame {
         }
         return a.length;
     }
-
+    
     public static String traduce_to_Ingles(String p) {
         int x = buscar(p, II);
         if (x != -1) {
@@ -258,7 +360,7 @@ public class Dc extends javax.swing.JFrame {
             return "No tenemos la traduccion";
         }
     }
-
+    
     public static String traduce_to_Español(String p) {
         int x = buscar(p, EE);
         if (x != -1) {
@@ -267,8 +369,8 @@ public class Dc extends javax.swing.JFrame {
             return "No tenemos la traduccion";
         }
     }
-
-        public static int buscar(String palabra, String m[]) {
+    
+    public static int buscar(String palabra, String m[]) {
         int i = 0;
         while (i < m.length) {
             if (palabra.equals(m[i])) {
@@ -278,7 +380,7 @@ public class Dc extends javax.swing.JFrame {
         }
         return -1;
     }
-
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -309,7 +411,7 @@ public class Dc extends javax.swing.JFrame {
                 new Dc().setVisible(true);
             }
         });
-
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -319,7 +421,7 @@ public class Dc extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txt1;
     // End of variables declaration//GEN-END:variables
